d1dd52cdf0c99d39687b0ec5cbefd6f2f6668980
trevor bikhram <tbikhram@trevors-MacBook-Pro.local>
2017-01-09 10:28:21 -0800

    survey Html update

--- server.js
+++ server.js
@@ -3,4 +3,21 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = 3000; 
+var PORT = process.env.PORT || 3000; 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+// Route files 
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+
+//listner
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
