c4d76c349d922b7eb3eadd90bf2c662289e2156c
trevor bikhram <tbikhram@trevors-MacBook-Pro.local>
2017-01-09 21:04:24 -0800

    html / data/ routes updated

---
+++ app/data/friends-data.js
@@ -0,0 +1,8 @@
+var friendArray =[{
+	name:"trevor",
+	email:"t.bikhram@gmail.com",
+	id: "tbik1234",
+	phoneNumber:"000-000-0000"
+}];
+
+module.exports = friendArray;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,15 @@
+var surveyData = require("../data/friend-data.js");
+
+//routing 
+
+module.exports = function(app){
+	app.get("/app/friends", function(req, res){
+		console.log()
+
+	});
+
+
+	app.post("/api/friends", function(req, res){
+
+	});
+}
